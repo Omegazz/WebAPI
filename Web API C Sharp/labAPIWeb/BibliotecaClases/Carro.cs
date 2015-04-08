57ec107bff4490700ea5ebd344d3715da5369927
Diego Salas <diegosalas.arce@gmail.com>
2015-04-08 14:13:04 -0600

    Inclusion del web api en C#
    Desarrollado en la Universidad Cenfotec como parte de un examen.

---
+++ Web API C Sharp/labAPIWeb/BibliotecaClases/Carro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BibliotecaClases
+{
+    public class Carro
+    {
+        #region Atributos
+
+        private int _ID;
+        private string _marca;
+        private string _anio;
+
+        #endregion
+
+        #region Constructores
+
+        public Carro (){}
+
+        public Carro(int pID, string pnombre, string papellido)
+        {
+            ID = pID;
+            Nombre = pnombre;
+            Apellidos = papellido;
+        }
+
+        #endregion
+
+        #region Sets y Gets
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return _anio; }
+            set { _anio = value; }
+        }
+
+        #endregion
+        
+    }
+}
---
+++ Web API C Sharp/labAPIWeb/MvcApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BibliotecaClases;
+
+namespace MvcApplication1.Controllers
+{
+
+    public class CarroController : ApiController
+    {
+
+        public static List<Carro> listadoCarros;
+
+        // GET api/values
+        public IEnumerable<Carro> Get()
+        {
+            if (listadoCarros == null)
+            {
+                listadoCarros = new List<Carro>();
+                llenarListado();
+            }
+
+            return listadoCarros;
+        }
+
+        // GET api/values/5
+        public Carro Get(int id)
+        {
+            return listadoCarros.FirstOrDefault((p) => p.ID == id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]int pid, [FromBody]string pmodelo, [FromBody]string panio)
+        {
+
+            listadoCarros.Add(new Carro(pid, pmodelo, panio));
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]Carro pcarro)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int pid)
+        {
+            listadoCarros.RemoveAt(pid);
+        }
+
+        public void llenarListado()
+        {
+            listadoCarros.Add(new Carro(0, "Toyota", "2000"));
+            listadoCarros.Add(new Carro(1, "Mitsubishi", "2015"));
+            listadoCarros.Add(new Carro(2, "Masseratti", "2012"));
+            listadoCarros.Add(new Carro(3, "Nissan", "2007"));
+            listadoCarros.Add(new Carro(4, "Renault", "2008"));
+            listadoCarros.Add(new Carro(5, "Hummer", "2012"));
+        }
+    }
+}
